ea310b86845bffbd676774bed154eecf18033b07
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-01-25 13:48:12 +0100

    Add files via upload

---
+++ TrackBox/TrackBox/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TrackBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trB1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBx1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trB1
+            // 
+            this.trB1.Location = new System.Drawing.Point(84, 94);
+            this.trB1.Maximum = 50;
+            this.trB1.Name = "trB1";
+            this.trB1.Size = new System.Drawing.Size(283, 56);
+            this.trB1.TabIndex = 0;
+            this.trB1.Scroll += new System.EventHandler(this.trB1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Postavljena vrijednost je:";
+            // 
+            // txtBx1
+            // 
+            this.txtBx1.Location = new System.Drawing.Point(252, 184);
+            this.txtBx1.Name = "txtBx1";
+            this.txtBx1.Size = new System.Drawing.Size(100, 22);
+            this.txtBx1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBx1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trB1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trB1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trB1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBx1;
+    }
+}
+
